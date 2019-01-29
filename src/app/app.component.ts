2040a04a8e4ce8ac7ec2336af4781c5af90e6392
Michael Gray <michaelgray@etech.local>
2019-01-29 14:33:08 +0000

    Added child component with on push amd mark for check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,28 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit {
   title = 'ObservableWithService';
-  time: Date;
+  time = Date.now();
+  mySubject = new Subject<any>();
 
   constructor(private appService: AppService) {
 
   }
   ngOnInit() {
     this.appService.myEventEmitter.subscribe(x => {
-    console.log("New Data...", x)
+      console.log('New Data...', x);
       this.time = x;
-    })
+
+      this.mySubject.next(x);
+    });
   }
 
-  getData(){
-    console.log("Fetching data...")
+  getData() {
+    console.log('Fetching data...');
     this.appService.myServiceFunction();
   }
 
-  stopData(){
-    console.log("Stopped")
+  stopData() {
+    console.log('Stopped');
     this.appService.myEventEmitter.unsubscribe();
     // this.time = Date.now()
     this.appService.resetEventEmmitter()
